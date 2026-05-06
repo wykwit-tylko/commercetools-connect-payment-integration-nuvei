61a51512413614262ba23867664356a7416707d6
Wiktor W. <wiktor.witkowski@tylko.com>
2026-05-07 00:16:07 +0200

    Update enabler build configuration for Connect deployment

--- enabler/vite.config.ts
+++ enabler/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  publicDir: false,
   build: {
+    outDir: resolve(__dirname, "public"),
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
       name: "NuveiEnabler",
-      fileName: "index",
+      fileName: (format) => `nuvei-enabler.${format}.js`,
       formats: ["es", "umd"],
     },
     rollupOptions: {
